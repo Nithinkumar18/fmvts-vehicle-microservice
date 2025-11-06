bf94251673272089ed88763100f684f00f5a1096
Nithin <vooturinithinkumar@gmail.com>
2025-11-06 17:02:20 +0530

    add logger

---
+++ vehicle/src/loggers/logger.js
@@ -0,0 +1,32 @@
+const {createLogger, transports, format} = require('winston');
+
+
+const customLevels = {
+    levels:{
+        error: 0,
+        info: 2,
+        http: 3
+    },
+
+    colors:{
+       error: 'red',
+       info: 'green',
+       http: 'blue'
+    }
+}
+
+
+const logger = createLogger({
+    levels: customLevels.levels,
+    level: 'http',
+    format: format.combine(
+            format.colorize(),
+            format.timestamp(),
+             format.printf(({timestamp,level,message}) => `${timestamp} ${level}: ${message}`)
+    ),
+    transports:[
+        new transports.Console()
+    ]
+})
+
+module.exports = logger
