1df685dad7b744a766cf00bbedb27c58eaeceb31
Nithin <vooturinithinkumar@gmail.com>
2025-11-06 17:03:38 +0530

    define schema

---
+++ vehicle/src/model/vehicle.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+const vehicleSchema = new mongoose.Schema({
+
+    VIN:{
+        type: String,
+        required:true,
+        unique: true
+    },
+
+    regNum:{
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    make:{
+        type: String,
+        required: true
+    },
+
+    model:{
+        type: String,
+        required: true
+    },
+
+    vehicleType:{
+        type: String,
+        required: true
+    },
+
+    fuelType:{
+        type: String,
+        required: true
+    },
+
+    capacity:{
+        type: Number,
+        required: true
+    },
+
+    yearOfManufacture:{
+        type: Number,
+        required: true
+    },
+
+    vehicleStatus:{
+      type: String,
+      enum: ["Active","In-Active"],
+      default: "In-Active"
+    },
+
+    userId:{
+       type: String,
+    }
+},{timestamps: true});
+
+module.exports = mongoose.model("vehicle",vehicleSchema);
+
