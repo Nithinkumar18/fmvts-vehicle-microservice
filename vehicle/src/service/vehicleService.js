5080c4194951952c51cc3fc2e768e8423fa9f29b
Nithin <vooturinithinkumar@gmail.com>
2025-11-18 15:35:37 +0530

    add changes

--- vehicle/src/service/vehicleService.js
+++ vehicle/src/service/vehicleService.js
@@ -1,7 +1,8 @@
 const vehicle = require('../model/vehicle');
 const rsInfo = require('../constants/responseInfo');
 const logger = require('../loggers/logger');
-const {fetchDriverProfile} = require('../interService/userClient');
+const { fetchDriverProfile } = require('../interService/userClient');
+const { sendDriverDetails } = require('../events/publishDriverDetails');
 let uuidv4;
 (async () => {
     const { v4 } = await import('uuid');
@@ -10,80 +11,81 @@ let uuidv4;
 
 
 const registerVehicle = async (vehicleInfo) => {
-    try{
-       let VIN = generateVIN();
-       const _vehicleDetails = {VIN,...vehicleInfo};
-       const _vehicle = await vehicle.create(_vehicleDetails);
-       logger.info(`SERVICE - ${rsInfo.SERVICE} : ${rsInfo.VEHICLE_REGESTRATION_SUCCESS}`);
-       logger.info(`Created Vehicle Info ${_vehicle.VIN}: created At: ${_vehicle.createdAt}`);
-       return _vehicle;
+    try {
+        let VIN = generateVIN();
+        const _vehicleDetails = { VIN, ...vehicleInfo };
+        const _vehicle = await vehicle.create(_vehicleDetails);
+        logger.info(`SERVICE - ${rsInfo.SERVICE} : ${rsInfo.VEHICLE_REGESTRATION_SUCCESS}`);
+        logger.info(`Created Vehicle Info ${_vehicle.VIN}: created At: ${_vehicle.createdAt}`);
+        return _vehicle;
 
     }
-    catch(err){
-       logger.error(`SERVICE - ${rsInfo.SERVICE} : ${rsInfo.VEHICLE_REGESTRATION_FAIL}`+ "  "+ err);
-       throw new Error(err);
-    }
-}
-
-
-const vehicleInfo = async(vehicleId) =>{
-    try{
-      const vInfo = await vehicle.findOne({VIN: vehicleId});
-      logger.info(`SERVICE - ${rsInfo.SERVICE} : ${rsInfo.SEARCH_VEHICLE_INFO}`);
-      return vInfo;
-    }
-    catch(err){
-      logger.error(`SERVICE - ${rsInfo.SERVICE} : ${rsInfo.SEARCH_VEHICLE_INFO}`+ "  "+ err);
-      throw new Error(err);
-    }
-}
-
-const updateVehicleInfo = async(v_id,vdata) => {
-    try{
-        const up_info = await vehicle.findByIdAndUpdate({'_id':v_id},{$set:vdata});
-        logger.info(`SERVICE - ${rsInfo.SERVICE} : ${rsInfo.VEHICLE_UPDATE_SUCCESS}`);
-        logger.info(`Updated Vehicle Info ${up_info.VIN}: updated At: ${up_info.updatedAt}`);
-        return up_info;
-    }
-    catch(err){
-        logger.error(`SERVICE - ${rsInfo.SERVICE} : ${rsInfo.VEHICLE_UPDATE_SUCCESS}`+ "  "+ err);
+    catch (err) {
+        logger.error(`SERVICE - ${rsInfo.SERVICE} : ${rsInfo.VEHICLE_REGESTRATION_FAIL}` + "  " + err);
         throw new Error(err);
     }
 }
 
 
-const retireVehicle = async(_id) => {
-
-    try{
-       const retired_Vehicle = await vehicle.findByIdAndDelete({'_id': _id});
-       logger.info(`SERVICE - ${rsInfo.SERVICE} : ${rsInfo.VEHICLE_RETIRED}`);
-       logger.info(`Updated Vehicle Info ${retired_Vehicle.VIN,retired_Vehicle.regNum}: updated At: ${retired_Vehicle.updatedAt}`);
-       return {
-         vehicleVIN: retired_Vehicle.VIN,
-         registrationNum: retired_Vehicle.regNum,
-         retiredOn: retired_Vehicle.updatedAt
-
-       };
+const vehicleInfo = async (vehicleId) => {
+    try {
+        const vInfo = await vehicle.findOne({ VIN: vehicleId });
+        logger.info(`SERVICE - ${rsInfo.SERVICE} : ${rsInfo.SEARCH_VEHICLE_INFO}`);
+        return vInfo;
     }
-    catch(err){
-       logger.error(`SERVICE - ${rsInfo.SERVICE} : ${rsInfo.VEHICLE_RETIRED}`+"  "+err);
-       throw new Error(err);
+    catch (err) {
+        logger.error(`SERVICE - ${rsInfo.SERVICE} : ${rsInfo.SEARCH_VEHICLE_INFO}` + "  " + err);
+        throw new Error(err);
+    }
+}
+
+const updateVehicleInfo = async (v_id, vdata) => {
+    try {
+        const up_info = await vehicle.findByIdAndUpdate({ '_id': v_id }, { $set: vdata });
+        logger.info(`SERVICE - ${rsInfo.SERVICE} : ${rsInfo.VEHICLE_UPDATE_SUCCESS}`);
+        logger.info(`Updated Vehicle Info ${up_info.VIN}: updated At: ${up_info.updatedAt}`);
+        return up_info;
+    }
+    catch (err) {
+        logger.error(`SERVICE - ${rsInfo.SERVICE} : ${rsInfo.VEHICLE_UPDATE_SUCCESS}` + "  " + err);
+        throw new Error(err);
+    }
+}
+
+
+const retireVehicle = async (_id) => {
+
+    try {
+        const retired_Vehicle = await vehicle.findByIdAndDelete({ '_id': _id });
+        logger.info(`SERVICE - ${rsInfo.SERVICE} : ${rsInfo.VEHICLE_RETIRED}`);
+        logger.info(`Updated Vehicle Info ${retired_Vehicle.VIN, retired_Vehicle.regNum}: updated At: ${retired_Vehicle.updatedAt}`);
+        return {
+            vehicleVIN: retired_Vehicle.VIN,
+            registrationNum: retired_Vehicle.regNum,
+            retiredOn: retired_Vehicle.updatedAt
+
+        };
+    }
+    catch (err) {
+        logger.error(`SERVICE - ${rsInfo.SERVICE} : ${rsInfo.VEHICLE_RETIRED}` + "  " + err);
+        throw new Error(err);
     }
 }
 
 const generateVIN = () => {
-    let vehicleIdenficationNumber = uuidv4().replace(/-/g,"").slice(0,16).toUpperCase();
+    let vehicleIdenficationNumber = uuidv4().replace(/-/g, "").slice(0, 16).toUpperCase();
     return vehicleIdenficationNumber;
 }
 
 const assignOrUnAssignDriver = async (vehicleIdNumber, driverId, action) => {
     try {
         if (action == rsInfo.UNASSIGN) {
-          const deallocateDriver = await vehicle.findOneAndUpdate({VIN:vehicleIdNumber},{$unset:{userId: " "}},{new:true});
-          logger.info(`SERVICE - ${rsInfo.SERVICE} : ${rsInfo.VEHICLE_UNASSIGNMENT_COMPLETED}`);
-          return {
-             unassignment_success: rsInfo.VEHICLE_UNASSIGNMENT_COMPLETED
-          };
+            const deallocateDriver = await vehicle.findOneAndUpdate({ VIN: vehicleIdNumber }, { $unset: { userId: " " } });
+            logger.info(`SERVICE - ${rsInfo.SERVICE} : ${rsInfo.VEHICLE_UNASSIGNMENT_COMPLETED}`);
+            await sendDriverDetails(deallocateDriver.userId, driverId);
+            return {
+                unassignment_success: rsInfo.VEHICLE_UNASSIGNMENT_COMPLETED,
+            };
         }
         else {
             const valid_vehicle = await vehicleInfo(vehicleIdNumber);
@@ -91,10 +93,12 @@ const assignOrUnAssignDriver = async (vehicleIdNumber, driverId, action) => {
                 if (valid_vehicle.vehicleStatus == rsInfo.VSTATUS) {
                     const dProfile = await fetchDriverProfile(driverId);
                     if (dProfile.availabilityStatus == rsInfo.DRIVER_STATUS) {
-                        const assignDriver = await vehicle.findOneAndUpdate({ VIN: vehicleIdNumber }, { $set:  { userId: driverId } });
+                        const assignDriver = await vehicle.findOneAndUpdate({ VIN: vehicleIdNumber }, { $set: { userId: driverId } }, { new: true });
                         logger.info(`SERVICE - ${rsInfo.SERVICE} : ${rsInfo.DRIVER_ASSIGNMENT_SUCCESS}`);
-                        const {fullName,contactNumber,licenseNumber} = dProfile;
-                        const {regNum,VIN} = assignDriver;
+                        const { fullName, contactNumber, licenseNumber } = dProfile;
+                        const { regNum, VIN } = assignDriver;
+                        console.log(assignDriver);
+                        await sendDriverDetails(assignDriver.userId, assignDriver._id);
                         return {
                             fullName,
                             contactNumber,
